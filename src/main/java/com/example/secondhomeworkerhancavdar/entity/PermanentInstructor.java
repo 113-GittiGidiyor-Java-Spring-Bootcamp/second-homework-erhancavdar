dfd1de6031906a4af0e9b8ec2a2df403f340b1ed
rhancav <edcavdar@gmail.com>
2021-08-16 13:21:50 +0300

    entities

--- src/main/java/com/example/secondhomeworkerhancavdar/entity/PermanentInstructor.java
+++ src/main/java/com/example/secondhomeworkerhancavdar/entity/PermanentInstructor.java
@@ -1,4 +1,31 @@
 package com.example.secondhomeworkerhancavdar.entity;
 
-public class PermanentInstructor {
+import javax.persistence.Entity;
+
+@Entity
+public class PermanentInstructor extends Instructor {
+    private float fixedSalary;
+
+    public PermanentInstructor() {
+    }
+
+    public PermanentInstructor(String name, String phoneNumber, float fixedSalary) {
+        super(name, phoneNumber);
+        this.fixedSalary = fixedSalary;
+    }
+
+    public float getFixedSalary() {
+        return fixedSalary;
+    }
+
+    public void setFixedSalary(float fixedSalary) {
+        this.fixedSalary = fixedSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "PermanentInstructor{" +
+                "fixedSalary=" + fixedSalary +
+                '}';
+    }
 }
